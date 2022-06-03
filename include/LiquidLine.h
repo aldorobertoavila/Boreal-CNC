9a18434651c51d43ee5be2652b1698d7d91f78a5
Aldo Avila <45251738+aldorobertoavila@users.noreply.github.com>
2022-06-03 14:43:05 -0700

    Refactor: Include

---
+++ include/LiquidLine.h
@@ -0,0 +1,26 @@
+#include <LiquidCrystal_I2C.h>
+
+class LiquidLine
+{
+public:
+    LiquidLine(uint8_t col, uint8_t row, String text);
+
+    uint8_t getColumn() const;
+
+    uint8_t getRow() const;
+
+    String getText();
+
+    void print(LiquidCrystal_I2C &lcd);
+
+    void setColumn(uint8_t col);
+
+    void setRow(uint8_t row);
+
+    void setText(String text);
+
+protected:
+    uint8_t _col;
+    uint8_t _row;
+    String _text;
+};
---
+++ include/LiquidScreen.h
@@ -0,0 +1,38 @@
+#include <LiquidCrystal_I2C.h>
+#include <LiquidLine.h>
+
+#define MAX_SCREENS 8
+#define MAX_LINES 16
+
+class LiquidScreen
+{
+public:
+    LiquidScreen();
+
+    void addLine(uint8_t id, LiquidLine &line);
+
+    LiquidLine *getCurrentLine();
+
+    void nextLine();
+
+    void previousLine();
+
+    void print(LiquidCrystal_I2C &lcd, uint8_t cols, uint8_t rows);
+
+    void setCurrentLine(uint8_t id);
+
+    void setFocusPosition(uint8_t col, uint8_t row);
+
+    void setFocusSymbol(uint8_t id);
+
+protected:
+    uint8_t _currentLine;
+    uint8_t _focusedCol;
+    uint8_t _focusedRow;
+    uint8_t _focusedLine;
+    uint8_t _lineCount;
+    LiquidLine *_lines[MAX_LINES];
+    uint8_t _prevFocusedCol;
+    uint8_t _prevFocusedRow;
+    uint8_t _symbol;
+};
